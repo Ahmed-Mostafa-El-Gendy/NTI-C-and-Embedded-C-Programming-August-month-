3159844ee0e3d419ed4f04e6c76a3a8d655d747e
Ahmed Mostafa El Gendy <121825618+Ahmed-Mostafa-El-Gendy@users.noreply.github.com>
2023-08-09 07:02:43 +0300

    Finsh Task

---
+++ Day 2/Assignments/Challenge1/challenge.c
@@ -0,0 +1,81 @@
+/*
+ ******************************************************************************
+ * @file           : Lab7.c
+ * @author         : Ahmed_Mostafa_El_Gendy
+ * @brief          : ___
+ * @date           : 7 / August / 2023 
+ ******************************************************************************
+ */
+#include<stdio.h>
+ int main ()
+ {
+	 int op1,op2,op3,op4,op5,op6,op7,op8,op9,op10,number ;
+	 printf("please enter the numers : \n");
+	 printf("please enter the op1 :");
+	 scanf("%d",&op1);
+	 printf("please enter the op2 :");
+	 scanf("%d",&op2);
+	 printf("please enter the op3 :");
+	 scanf("%d",&op3);
+	 printf("please enter the op4 :");
+	 scanf("%d",&op4);
+	 printf("please enter the op5 :");
+	 scanf("%d",&op5);
+	 printf("please enter the op6 :");
+	 scanf("%d",&op6);
+	 printf("please enter the op7 :");
+	 scanf("%d",&op7);
+	 printf("please enter the op8 :");
+	 scanf("%d",&op8);
+	 printf("please enter the op9 :");
+	 scanf("%d",&op9);
+	 printf("please enter the op10 :");
+	 scanf("%d",&op10);
+	 printf("please enter Enter the value :");
+	 scanf("%d",&number);
+	 if(number==op1)
+	 {
+		 printf("the value exist in number1 \n");
+	 }
+	  if (number==op2)
+	 {
+		 printf("the value exist in number2 \n");
+	 }
+	  if (number==op3)
+	 {
+		 printf("the value exist in number3 \n");
+	 }
+	  if (number==op4)
+	 {
+		 printf("the value exist in number4 \n");
+	 }
+	  if (number==op5)
+	 {
+		 printf("the value exist in number5 \n");
+	 }
+	  if (number==op6)
+	 {
+		 printf("the value exist in number6 \n");
+	 }
+	  if (number==op7)
+	 {
+		 printf("the value exist in number7 \n");
+	 }
+	  if (number==op8)
+	 {
+		 printf("the value exist in number8 \n");
+	 }
+	  if (number==op9)
+	 {
+		 printf("the value exist in number9 \n");
+	 }
+	  if (number==op10)
+	 {
+		 printf("the value exist in number10 \n");
+	 }
+	 else 
+	 {
+		 printf("the value dosen't exist \n");
+	 }
+	 printf("thank you ");
+ } 
