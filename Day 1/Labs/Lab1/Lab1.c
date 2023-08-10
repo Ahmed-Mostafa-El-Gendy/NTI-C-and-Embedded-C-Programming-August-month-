4db25b694460114bdfff57453666954da7e17923
Ahmed Mostafa El Gendy <121825618+Ahmed-Mostafa-El-Gendy@users.noreply.github.com>
2023-08-10 05:40:57 +0300

    c programming

---
+++ Day 1/Labs/Lab1/Lab1.c
@@ -0,0 +1,14 @@
+/*
+ ******************************************************************************
+ * @file           : Lab1.c
+ * @author         : Ahmed_Mostafa_El_Gendy
+ * @brief          : How to write hello world.
+ * @date           : 6 / 8 / 2023 
+ ******************************************************************************
+ */
+#include<stdio.h>
+int main ()
+{
+	printf("hello world");
+	return 0 ;
+}
---
+++ Day 1/Labs/Lab2/Lab2.c
@@ -0,0 +1,16 @@
+/*
+ ******************************************************************************
+ * @file           : Lab2.c
+ * @author         : Ahmed_Mostafa_El_Gendy
+ * @brief          : How to write my info by using scape operator .
+ * @date           : 6 / 8 / 2023 
+ ******************************************************************************
+ */
+#include<stdio.h>
+int main ()
+{
+	printf("Ahmed Mostafa El Gendy \n");
+	printf("I am 21 years old \n");
+	printf("I am student in mansoura university engineering \n");
+	printf("graduated mansoura university 2025 \n");
+}
